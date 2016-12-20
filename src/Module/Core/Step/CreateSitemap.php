cced399547f117b4f1a0adf1c51e3d429c89fcf7
Chris Bay <chris@chrisbay.net>
2016-12-20 11:50:39 -0600

    Partial work on HTML sitemap

--- src/Module/Core/Step/CreateSitemap.php
+++ src/Module/Core/Step/CreateSitemap.php
@@ -20,9 +20,10 @@ class CreateSitemap implements Step
     private $logger;
 
     /**
-     * @var Sitemap file
+     * @var Simple sitemap file (URL list)
      */
-    private $sitemapFilename = "sitemap.txt";
+    private $simpleSitemapFilename = "sitemap.txt";
+    private $htmlSitemapFilename = "sitemap.html";
 
     public function __construct(LoggerInterface $logger)
     {
@@ -31,16 +32,85 @@ class CreateSitemap implements Step
 
     public function __invoke(Project $project)
     {
-        $sitemap = fopen($project->targetDirectory.'/'.$this->sitemapFilename, 'w');
-        $metadata = $project->metadata;
+        $this->generateSimpleSitemap($project);
+        $this->generateHtmlSitemap($project);
+    }
 
-        foreach ($metadata['pageList'] as $page) {
-            $this->logger->debug('Adding {page} to sitemap', ['page' => $page]);
-            $pageUrl = $metadata['baseUrl'].'/'.$page;
-            fwrite($sitemap, $pageUrl.PHP_EOL);
+    private function generateSimpleSitemap(Project $project)
+    {
+        $sitemap = fopen($project->targetDirectory.'/'.$this->simpleSitemapFilename, 'w');
+
+        if ($sitemap) {
+            $metadata = $project->metadata;
+
+            print_r($metadata);
+
+            foreach ($metadata['pageList'] as $page) {
+                $this->logger->debug('Adding {page} to simple sitemap', ['page' => $page]);
+                $pageUrl = $metadata['baseUrl'].'/'.$page;
+                fwrite($sitemap, $pageUrl.PHP_EOL);
+            }
+
+            fclose($sitemap);
+        } else {
+            $this->logger->error('Failed to open simple sitemap file: {file}. No sitemap generated.', ['file' => $this->simpleSitemapFilename]);
         }
 
-        fclose($sitemap);
+    }
 
+    private function generateHtmlSitemap(Project $project)
+    {
+        $sitemap = fopen($project->targetDirectory.'/'.$this->htmlSitemapFilename, 'w');
+
+        if ($sitemap) {
+            $metadata = $project->metadata;
+
+            $head = '<html>'.
+                        '<head>'.
+                            '<title>Sitemap</title>'.
+                        '</head>'.
+                        '<body>'.
+                            '<h1>Sitemap</h1>';
+
+            $foot = '</body></html>';
+
+            fwrite($sitemap, $head);
+            fwrite($sitemap, $this->parseSubtree($metadata['pageTree'], ''))
+            foreach ($metadata['pageList'] as $page) {
+                $this->logger->debug('Adding {page} to HTML sitemap', ['page' => $page]);
+                $pageUrl = $metadata['baseUrl'].'/'.$page;
+                fwrite($sitemap, $pageUrl.PHP_EOL);
+            }
+
+            fwrite($sitemap, $foot);
+
+            fclose($sitemap);
+        } else {
+            $this->logger->error('Failed to open HTML sitemap file: {file}. No sitemap generated.', ['file' => $this->$htmlSitemapFilename]);
+        }
+
+    }
+
+    private function parseSubtree($subtree, $markup)
+    {
+        $markup .= '<ul>';
+
+        foreach ($subtree as $item) {
+
+            // check for type of $item
+
+            // if string, add to markup
+
+            // if array, parse
+
+        }
+
+        $markup .= '</ul>';
+        return $markup;
+    }
+
+    private function createSiteLink($href, $text)
+    {
+        return "<li><a href='$href' >$text</a></li>";
     }
 }
